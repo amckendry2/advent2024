7b192d4628c6b83b42ab980ac6cd807a8d269c0d
Alex <a.mckendry2@gmail.com>
2024-12-09 16:09:38 +0300

    day 3

--- 3/solution.py
+++ 3/solution.py
@@ -2,13 +2,12 @@ import sys
 import re
 
 def is_enabled(instr, index):
-    return instr[max(0, next((i for i in range(len(instr)) if instr[i][0] > index), len(instr)) - 1)][1] == "on"
+    idx = next((i for i in range(len(instr)) if instr[i][0] > index), len(instr)) - 1
+    return True if idx == -1 else instr[max(0, idx)][1] == "on"
 
 text = sys.stdin.read()
-dos = [(m.start(), "on") for m in re.finditer(r'do\(\)', text)]
-donts = [(m.start(), "off") for m in re.finditer(r'don\'t\(\)', text)]
-instructions = sorted(dos + donts)
-matches_iter = re.finditer(r'mul\((\d+),(\d+)\)', text)
-# matches = list(matches_iter)
-# print("Part 1: ", sum([int(m.groups(0)[0]) * int(m.groups(0)[1]) for m in matches]))
-print("Part 2: ", sum([(int(m.groups(0)[0]) * int(m.groups(0)[1]) if is_enabled(instructions, m.start()) else 0) for m in matches_iter]))
+instructions = sorted([(m.start(), "on") for m in re.finditer(r'do\(\)', text)] +
+                      [(m.start(), "off") for m in re.finditer(r'don\'t\(\)', text)])
+mults = [(m.start(), m) for m in re.finditer(r'mul\((\d+),(\d+)\)', text)]
+print("Part 1: ", sum([int(m[1].groups(0)[0]) * int(m[1].groups(0)[1]) for m in mults]))
+print("Part 2: ", sum([int(m[1].groups(0)[0]) * int(m[1].groups(0)[1]) if is_enabled(instructions, m[0]) else 0 for m in mults]))
